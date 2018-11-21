a46d897101dff83a3e6695636496f81e9d1b8290
keepthinkingit <781130764@qq.com>
2018-11-22 00:50:41 +0800

    finish login and logout

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $result = $this->validate($request, [
+            'email' => 'required|email|max:200',
+            'password' => 'required'
+        ]);
+
+        $data = ['email' => $request->email,
+                'password' => $request->password];
+        if(Auth::attempt($data, $request->has('remember'))){
+            session()->flash('success', ' Welcome back!');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else{
+            session()->flash('danger', 'email or password input wrong , please check it !');
+            return redirect()->back();
+        }
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', 'You have logout , thanks ~');
+        return redirect('login');
+    }
+}
